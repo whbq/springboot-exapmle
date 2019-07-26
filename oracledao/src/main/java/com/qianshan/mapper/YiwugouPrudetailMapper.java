22126ee1755fb537afaf1102bdd936b925b84689
13033714803@163.com <wuhongbo@123>
2019-07-26 17:34:00 +0800

    1

---
+++ oracledao/src/main/java/com/qianshan/mapper/YiwugouPrudetailMapper.java
@@ -0,0 +1,7 @@
+package com.qianshan.mapper;
+
+import com.qianshan.modal.YiwugouPrudetail;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface YiwugouPrudetailMapper extends Mapper<YiwugouPrudetail> {
+}
